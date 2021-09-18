8f10aa8a9f8fafe3bef24bcb6634df8f193af4b6
amandafyfy <amandafyfy@gmail.com>
2021-09-18 15:10:32 +0800

    Adding logic for integrating files

--- Data Integrator with GUI/integrator/integrator.py
+++ Data Integrator with GUI/integrator/integrator.py
@@ -33,19 +33,36 @@ class Integrator(QObject):
         self.progressed.emit(0)  # Reset the progress
         self.finished.emit()
         cmd = '''
-        mkdir output
 
-        head -1 data/records1.csv > output/all_records_final.csv
-        tail -n +2 -q data/records*.csv >> output/all_records_final.csv
+        head -1 data/records1.csv > output/finalData.csv
+        tail -n +2 -q data/records*.csv >> output/finalData.csv
 
-        grep -q "" data/locations.csv
-        if [[ $? != 0 ]] ; then
+        FILE_loc=data/locations.csv
+        FILE_garmin=data/garmin.csv
+        FILE_vis=data/visited_places.csv
+
+        if [[ -f "$FILE_vis" && -f "$FILE_loc" && -f "$FILE_garmin" ]]; then
             sqlite3 < sqlscript-no-locations.txt
-        else
             sqlite3 < sqlscript.txt
+            sqlite3 < sqlscript_garmin.txt
+        elif [[ -f "$FILE_vis" && -f "$FILE_loc" ]]; then
+            sqlite3 < sqlscript-no-locations.txt
+            echo "Amanda"
+            cat output/finalData.csv
+            sqlite3 < sqlscript.txt
+        elif [[ -f "$FILE_vis" && -f "$FILE_garmin" ]]; then
+            sqlite3 < sqlscript-no-locations.txt
+            sqlite3 < sqlscript_garmin.txt
+        elif [[ -f "$FILE_loc" && -f "$FILE_garmin" ]]; then
+            sqlite3 < sqlscript.txt
+            sqlite3 < sqlscript_garmin.txt
+        elif [ -f "$FILE_vis" ]; then
+            sqlite3 < sqlscript-no-locations.txt
+        elif [ -f "$FILE_loc" ]; then
+            sqlite3 < sqlscript.txt
+        elif [ -f "$FILE_garmin" ]; then
+            sqlite3 < sqlscript_garmin.txt
         fi
-
-        mkdir data/Integrated\ Data
 
         mv data/*.csv data/Integrated\ Data/
 
