795da73edb0ce5d7e6d95da89ed104b3b496ac4d
Han <56809330+Hanchooi@users.noreply.github.com>
2021-09-24 12:55:52 +0800

    Update merge_visited.sql

--- Data Integrator with GUI/merge_visited.sql
+++ Data Integrator with GUI/merge_visited.sql
@@ -5,23 +5,18 @@
 
 .output output/finalData.csv
 
-SELECT R.*, VP.locationName AS vis_name, 
-        VP.Activity1 AS vis_activity1, VP.Activity2 AS vis_activity2, VP.Activity3 AS vis_activity3, 
-        VP.latitude AS vis_lat, VP.longitude AS vis_long
+SELECT 
+R.*, CASE
+	WHEN speed >= 25 AND speed <= 28 THEN 'Bus'
+	WHEN speed >= 13 AND speed <= 25 THEN 'Car'
+	WHEN speed >= 1.5 AND speed <= 2.5 THEN 'Walk'
+	WHEN speed >= 2.5 AND speed <= 4.1 THEN'Running'
+	ELSE 'Train'
+END AS 'mode_of_transport', VP.locationName AS vis_name, 
+VP.Activity1 AS vis_activity1, VP.Activity2 AS vis_activity2, VP.Activity3 AS vis_activity3,VP.latitude AS vis_lat, VP.longitude AS vis_long
 FROM records AS R
 LEFT JOIN visitedPlaces AS VP
     ON ROUND(R.latitude,4) = ROUND(VP.latitude,4)
         AND ROUND(R.longitude,4) = ROUND(VP.longitude,4);
- 
-SELECT speed, 
- CASE
-           WHEN speed >= 25 AND speed <= 28 THEN 'Bus'
-           when speed >= 13 AND speed <= 25 THEN 'Car'
-           when speed >= 1.5 AND speed <= 2.5 THEN 'Walk'
-           when speed >= 2.5 AND speed <= 4.1 THEN'Running'
-           ELSE  
-                'Train'
-           END AS 'mode_of_transport'
-FROM records;
 
 .output stdout
