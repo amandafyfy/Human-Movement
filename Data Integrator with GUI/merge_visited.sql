3fb1a6b53fe402e37a1e7f20b385a684ca3785b7
Han <56809330+Hanchooi@users.noreply.github.com>
2021-10-19 07:53:08 +0800

    minor fix merge_visited

--- Data Integrator with GUI/merge_visited.sql
+++ Data Integrator with GUI/merge_visited.sql
@@ -8,8 +8,8 @@
 SELECT R.*, VP.latitude AS vis_lat, VP.longitude AS vis_long,
 VP.locationName AS vis_locName, VP.transport AS vis_transport,
 VP."Activity 1" AS vis_act1, VP.Enjoyment1 AS vis_act_enjoyment1,
-VP."Activity 2" AS vis_act2, VP.Enjoyment1 AS vis_act_enjoyment2,
-VP."Activity 3" AS vis_act3, VP.Enjoyment1 AS vis_act_enjoyment3,
+VP."Activity 2" AS vis_act2, VP.Enjoyment2 AS vis_act_enjoyment2,
+VP."Activity 3" AS vis_act3, VP.Enjoyment3 AS vis_act_enjoyment3,
 VP.Comment AS vis_comment
 FROM records AS R
 LEFT JOIN visitedPlaces AS VP
