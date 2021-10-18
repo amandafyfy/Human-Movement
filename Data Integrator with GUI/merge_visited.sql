364618613ef5671e2a10653d38b77f75d109d538
Han <56809330+Hanchooi@users.noreply.github.com>
2021-10-19 07:51:54 +0800

    Update merge_visited.sql

--- Data Integrator with GUI/merge_visited.sql
+++ Data Integrator with GUI/merge_visited.sql
@@ -5,7 +5,13 @@
 
 .output output/finalData.csv
 
-SELECT R.*, VP.latitude AS vis_lat, VP.longitude AS vis_long, VP.* FROM records AS R
+SELECT R.*, VP.latitude AS vis_lat, VP.longitude AS vis_long,
+VP.locationName AS vis_locName, VP.transport AS vis_transport,
+VP."Activity 1" AS vis_act1, VP.Enjoyment1 AS vis_act_enjoyment1,
+VP."Activity 2" AS vis_act2, VP.Enjoyment1 AS vis_act_enjoyment2,
+VP."Activity 3" AS vis_act3, VP.Enjoyment1 AS vis_act_enjoyment3,
+VP.Comment AS vis_comment
+FROM records AS R
 LEFT JOIN visitedPlaces AS VP
     ON ROUND(R.latitude,4) = ROUND(VP.latitude,4)
         AND ROUND(R.longitude,4) = ROUND(VP.longitude,4);
